2b0b08c786ef2fe7dfc81a12bcb8850526dcf8b1
danielmoret <dmoret17@gmail.com>
2023-03-01 09:21:10 -0400

    home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,54 @@
 import React from "react";
+import marvelLogo from "../../img/Marvel_Logo.svg.png";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-md bg-dark py-3">
+      <div className="container ">
+        <Link to="/">
+          <img src={marvelLogo} className="logo-navbar"></img>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon">
+            <span className="" role="button">
+              <i
+                className="fa fa-bars"
+                aria-hidden="true"
+                style={{ color: "white" }}
+              ></i>
+            </span>
+          </span>
+        </button>
+        <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
+          <div className="w-100 d-flex justify-content-end">
+            <div className="navbar-nav ">
+              <Link
+                className="nav-link text-white fw-bold"
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </Link>
+              <Link className="nav-link text-white fw-bold" to="/">
+                Features
+              </Link>
+              <Link className="nav-link text-white fw-bold" to="/">
+                Pricing
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
